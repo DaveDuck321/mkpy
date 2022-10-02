f073bac9b1ef63c86f99233c6367fa2dd49fc559
Tom Grant <thomas.grant.mail@gmail.com>
2022-10-02 01:56:40 +0100

    Add threadpool-based rule executor
    This is an initial implementation of the make-like dependency resolution.
    
    Note: timestamps are not yet checked.

--- mkpy/make.py
+++ mkpy/make.py
@@ -1,7 +1,11 @@
+import multiprocessing
 import re
 
+from collections import defaultdict
+from enum import Enum
 from functools import partial
-from typing import Callable, NamedTuple
+from threading import Thread
+from typing import Callable, Mapping, NamedTuple
 
 
 class Requirements(NamedTuple):
@@ -104,3 +108,70 @@ def generate_dependency_graph(
         raise last_missing_target or MissingTargetException(target_name)
 
     return top_level
+
+
+class MakeBlockedException(Exception):
+    pass
+
+
+class MakeFinishedException(Exception):
+    pass
+
+
+class MakeState(Enum):
+    NOT_YET_MADE = 0
+    CURRENTLY_MAKING = 1
+    FINISHED_MAKING = 2
+
+
+target_states: Mapping[str, MakeState] = defaultdict(lambda: MakeState.NOT_YET_MADE)
+
+
+def get_next_node_to_build(top_level: Node):
+    # Note: GIL abuse
+    if target_states[top_level.name] != MakeState.NOT_YET_MADE:
+        raise MakeFinishedException()
+
+    waiting_for_depend = False
+    for node in top_level.depends + top_level.prerequisites:
+        if target_states[node.name] == MakeState.NOT_YET_MADE:
+            try:
+                return get_next_node_to_build(node)
+            except MakeBlockedException:
+                pass
+        if target_states[node.name] == MakeState.CURRENTLY_MAKING:
+            waiting_for_depend = True
+
+    if waiting_for_depend:
+        raise MakeBlockedException()
+
+    return top_level
+
+
+def worker_thread(top_level: Node):
+    while True:
+        try:
+            next_node = get_next_node_to_build(top_level)
+        except MakeFinishedException:
+            break
+
+        target_states[next_node.name] = MakeState.CURRENTLY_MAKING
+
+        # TODO: check timestamps when appropriate
+        next_node.recipe(next_node.name, next_node.depends, next_node.prerequisites)
+
+        target_states[next_node.name] = MakeState.FINISHED_MAKING
+
+
+def make(target_name, thread_count=multiprocessing.cpu_count()):
+    graph = generate_dependency_graph(target_name, satisfied_targets=set())
+
+    thread_pool: list[Thread] = []
+    for _ in range(thread_count):
+        thread_pool.append(Thread(target=partial(worker_thread, graph)))
+
+    for thread in thread_pool:
+        thread.start()
+
+    for thread in thread_pool:
+        thread.join()
