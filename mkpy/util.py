092d2d8efac31f0093d50ef07d5fb5ef19db8deb
Tom Grant <thomas.grant.mail@gmail.com>
2022-10-07 02:49:08 +0100

    Add 'format_list_with_regex' helpers for common substitutions

--- mkpy/util.py
+++ mkpy/util.py
@@ -1,6 +1,8 @@
+import re
 import subprocess
 import sys
 
+from functools import partial
 from pathlib import Path
 from types import TracebackType
 
@@ -25,6 +27,15 @@ def trim_library_code_from_traceback(
 def sh(command: str):
     print(command)
     subprocess.check_call(command, shell=True)
+
+
+def format_with_regex(regex: str, format_str: str, input_string: str):
+    match = re.compile(regex).fullmatch(input_string)
+    return format_str.format(*match.groups())
+
+
+def format_list_with_regex(regex: str, format_str: str, input_strings: list[str]):
+    return list(map(partial(format_with_regex, regex, format_str), input_strings))
 
 
 def log_message(message: str):
